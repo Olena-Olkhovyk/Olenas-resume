3fdf57fb11621a706a6c41da1b725cf01c2ce022
Olena <alyonaeremen@gmail.com>
2023-05-26 12:07:38 +0000

    Improve form for better validation

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@ const navBtnImg = document.querySelector('#nav-btn-img');
 
 const form=document.getElementById("main-form");
 form.addEventListener("submit", checkForm);
-let fail = "";
 
 navBtn.onclick= () => {
     if (nav.classList.toggle("open")){
@@ -14,18 +13,43 @@ navBtn.onclick= () => {
      navBtnImg.src = './img/NAV.svg';
     }
 }
-
-
+//Display error message
+function printError(elemId,hintMsg){
+    document.getElementById(elemId).innerHTML=hintMsg;
+}
+//Validation form
 function checkForm(e){
     e.preventDefault();
     const name=document.getElementById("name").value;
     const email=document.getElementById("email").value;
-    const phone=document.getElementById("text").value;
-    if(name == "" || email == "" || phone == ""){
-    fail="Please fill in all fields";
+    const phone=document.getElementById("phone").value;
+    let nameErr=(emailErr=mobileErr=true);
+//Checking name 
+    if(name == "") {
+        printError("nameErr", "Please enter your name");
+    }  else {
+            printError("nameErr", "");
+            nameErr = false;
+        }
+//Checking email
+        if(email==""){
+            printError("emailErr", "Please enter your email");
+        }else{
+            printError("emailErr", "");
+            emailErr=false;
+        }
+//Checking mobile phone
+        if(phone==""){
+            printError("mobileErr", "Please enter your mobile phone");
+        }else{
+            let equat=/^[\d\+][\d\(\)\ -]{4,14}\d$/;
+            if(equat.test(phone)===false){
+                printError("mobileErr", "Please enter a valid number");
+            }else{
+                printError("mobileErr","");
+                mobileErr=false;
+            }
+        }
     }
 
-    if(fail!=""){
-        document.getElementById("error").innerHTML=fail;
-    }
-}
+
